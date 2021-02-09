cd0fee42e4b9c8efdec10b9d881f5b13e66b273d
Dario Aceituno <darioaceituno@gmail.com>
2021-02-08 21:03:25 -0600

    More of Styled Components

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,6 +1,21 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer; 
+  
+  &:hover {
+      background-color: lightGreen;
+      color: black;
+  }
+`;
 
 class App extends Component {
     state = {
@@ -84,15 +99,14 @@ class App extends Component {
         }
 
         return (
-                <div className="App">
-                    <h1>Hi, I'm a React App</h1>
-                    <p className={classes.join(' ')}>This is really working!</p>
-                    <button
-                        style={style}
-                        onClick={this.togglePersonsHandler}>Toggle Persons
-                    </button>
-                    {persons}
-                </div>
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p className={classes.join(' ')}>This is really working!</p>
+                <StyledButton
+                    onClick={this.togglePersonsHandler}>Toggle Persons
+                </StyledButton>
+                {persons}
+            </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
