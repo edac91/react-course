fafc7b6e7fe3f8ef7b159d0ce175482da4263fc6
Dario Aceituno <darioaceituno@gmail.com>
2021-02-14 23:24:49 -0600

    Component creation lifecycle in Action

--- test-app/src/components/Persons/Person/Person.js
+++ test-app/src/components/Persons/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Person.module.css';
 
 const person = ( props ) => {
-
+    console.log('[]')
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
