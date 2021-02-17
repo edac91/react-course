f4c342cdfd235faaaed8031486476b2958d69ea2
Dario Aceituno <darioaceituno@gmail.com>
2021-02-16 22:28:25 -0600

    Using shouldComponentUpdate for optimization

--- test-app/src/components/Persons/Persons.js
+++ test-app/src/components/Persons/Persons.js
@@ -13,8 +13,11 @@ class Persons extends Component {
     // }
 
     shouldComponentUpdate(nextProps, nextState, nextContext) {
-        console.log('[Persons.js] shouldComponentUpdate')
-        return true;
+        console.log('[Persons.js] shouldComponentUpdate');
+        if (nextProps.persons !== this.props.persons)
+            return true;
+        else
+            return false;
     }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
