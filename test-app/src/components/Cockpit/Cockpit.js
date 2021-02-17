c96be65ff3166f0523233d1fabf54edfa5d1f4d6
Dario Aceituno <darioaceituno@gmail.com>
2021-02-16 22:02:45 -0600

    Cleaning up with lifecycle hooks and useEffect

--- test-app/src/components/Cockpit/Cockpit.js
+++ test-app/src/components/Cockpit/Cockpit.js
@@ -7,8 +7,18 @@ const Cockpit = ( props ) => {
         console.log('[Cockpit.js] useEffect');
         setTimeout(() => {
             alert('Saved data to cloud!');
-        }, 1000)
+        }, 1000);
+        return () => {
+            console.log('[Cockpit.js] cleanup work in useEffect');
+        };
     }, []);
+
+    useEffect(() => {
+        console.log('[Cockpit.js] 2nd useEffect');
+        return () => {
+            console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+        };
+    });
 
     const assignedClasses = [];
     let btnClass = '';
