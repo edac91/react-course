731c862a9f15bdf44725106d93599aef01912baa
Dario Aceituno <darioaceituno@gmail.com>
2021-02-18 00:05:54 -0600

    Using refs

--- test-app/src/components/Persons/Person/Person.js
+++ test-app/src/components/Persons/Person/Person.js
@@ -5,18 +5,31 @@ import withClass from "../../../hoc/withClass";
 
 class Person extends Component {
 
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    };
+
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+    };
+
     render() {
         console.log('[Person.js] rendering...');
         return (
             <Fragment>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input
+                    type="text"
+                    ref={this.inputElementRef}
+                    onChange={this.props.changed}
+                    value={this.props.name}/>
             </Fragment>
         );
-    }
+    };
 
-}
+};
 
 Person.propTypes = {
     click: PropTypes.func,
