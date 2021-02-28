7f44677eea751b3311e7946479f7d7acfc093d9a
Dario Aceituno <darioaceituno@gmail.com>
2021-02-27 18:49:26 -0600

    Working on Responsive Adjustments

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,9 +7,11 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 const SideDrawer = () => {
     return (
         <div className={classes.SideDrawer}>
-            <Logo />
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
             <nav>
-                <NavigationItems />
+                <NavigationItems/>
             </nav>
         </div>
     );
