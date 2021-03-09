8a2133b18394acde368486edf191e5e4f4a1907d
Dario Aceituno <darioaceituno@gmail.com>
2021-03-09 01:39:51 -0600

    Navigation Programmatically

--- routing--01-start/src/containers/Posts/Posts.js
+++ routing--01-start/src/containers/Posts/Posts.js
@@ -33,7 +33,8 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        //this.setState({selectedPostId: id});
+        this.props.history.push({pathname: '/' + id});
     }
 
     render() {
@@ -41,12 +42,14 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
+                    // <Link to={'/' + post.id} key={post.id}>
                         <Post
+                            key={post.id}
                             title={post.title}
                             author={post.author}
                             clicked={() => this.postSelectedHandler(post.id)}/>
-                    </Link>);
+                    //</Link>
+                );
             });
         }
 
