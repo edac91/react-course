653e2eaf6fca53e41d64dc737de762de83eea864
Dario Aceituno <darioaceituno@gmail.com>
2021-03-11 23:37:01 -0600

    Redirecting Requests

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from "../Posts/Posts";
@@ -26,6 +26,7 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
                     <Route path="/posts/" component={Posts} />
+                    <Redirect from="/" to="/posts" />
                 </Switch>
                 {/*<section>*/}
                 {/*    <FullPost id={this.state.selectedPostId} />*/}
