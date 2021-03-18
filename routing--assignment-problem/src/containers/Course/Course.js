366749431e747e779a81ab1fab96d6634b9f56d5
Dario Aceituno <darioaceituno@gmail.com>
2021-03-17 22:12:29 -0600

    Assignment 3 - Completed

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 class Course extends Component {
     render () {
+
+        let title = this.props.match.params.title;
+        let id = this.props.match.params.id;
+
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{title}</h1>
+                <p>You selected the Course with ID: {id}</p>
             </div>
         );
     }
