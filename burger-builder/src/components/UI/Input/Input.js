ec60f91c0aabb2763a85b38a0e200cfb4a24e017
Dario Aceituno <darioaceituno@gmail.com>
2021-04-16 22:45:51 -0600

    Adding a Dropdown Component

--- burger-builder/src/components/UI/Input/Input.js
+++ burger-builder/src/components/UI/Input/Input.js
@@ -11,19 +11,32 @@ const Input = (props) => {
             inputElement = <input
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value} />;
+                value={props.value}/>;
             break;
         case ('textarea'):
             inputElement = <textarea
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value} />;
+                value={props.value}/>;
+            break;
+        case ('select'):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    {...props.elementConfig}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>);
             break;
         default:
             inputElement = <input
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value} />;
+                value={props.value}/>;
     }
 
     return (
